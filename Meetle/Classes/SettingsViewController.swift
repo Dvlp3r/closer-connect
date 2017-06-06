37496bae106fcee2c84924d72bf126021d6f5b60
Mahendra Singh <mahendrachouhan1@gmail.com>
2017-06-06 14:22:33 +0530

    Settings screen UI connection with realtime database, Some issue while saving settings is fixed. App will show users data based on the settings data.

--- Meetle/Classes/SettingsViewController.swift
+++ Meetle/Classes/SettingsViewController.swift
@@ -7,17 +7,23 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
+import NVActivityIndicatorView
 
-class SettingsViewController: BaseViewController {
+class SettingsViewController: BaseViewController, NVActivityIndicatorViewable {
 	
-	@IBOutlet weak var userSex: UISegmentedControl?
-	@IBOutlet weak var userSearch: UISegmentedControl?
-	@IBOutlet weak var distance: UISegmentedControl?
-	@IBOutlet weak var sliderView: UIView?
-	@IBOutlet weak var rangeLabel: UILabel?
-	@IBOutlet weak var distanceLabel: UILabel?
+	@IBOutlet weak var userSex: UISegmentedControl!
+	@IBOutlet weak var userSearch: UISegmentedControl!
+	@IBOutlet weak var distance: UISegmentedControl!
+    @IBOutlet weak var distanceSlider: UISlider!
+	@IBOutlet weak var sliderView: UIView!
+	@IBOutlet weak var rangeLabel: UILabel!
+	@IBOutlet weak var distanceLabel: UILabel!
 	
-	fileprivate var slider: RangeSlider?
+    
+    var ref: DatabaseReference!
+	fileprivate var slider: RangeSlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +36,88 @@ class SettingsViewController: BaseViewController {
 		distance!.layer.borderWidth = UISegmentedControlBorderWidth
 		
 		initRangeSlider()
+        
+        ref = Database.database().reference()
+        self.ref.child("users").child((Auth.auth().currentUser?.uid)!).observe(.value, with: { (snapshot) in
+            
+            // Success
+            let value = snapshot.value as? NSDictionary
+            //let username = value?["username"] as? String ?? ""
+            //let user = User.init(username: username)
+            //print(value!)
+            if (value != nil)
+            {
+                print("You have successfully logged in")
+                if let settings = value?.object(forKey: "Settings" as NSString)
+                {
+                    if let gender = value?.object(forKey: "Gender" as NSString)
+                    {
+                        self.userSex.selectedSegmentIndex = gender as! Int
+                    }
+                    if let Intrests = (settings as AnyObject).object(forKey: "Intrests" as NSString)
+                    {
+                        self.userSearch.selectedSegmentIndex = Intrests as! Int
+                    }
+                    else
+                    {
+                        if let gender = value?.object(forKey: "Gender" as NSString)
+                        {
+                            self.userSex.selectedSegmentIndex = gender as! Int
+                            if (self.userSex.selectedSegmentIndex==1)
+                            {
+                                self.userSearch.selectedSegmentIndex = 0
+                            }
+                            else
+                            {
+                                self.userSearch.selectedSegmentIndex = 1
+                            }
+                            
+                        }
+                        
+                    }
+                    if let Distance = (settings as AnyObject).object(forKey: "Distance" as NSString)
+                    {
+                        self.distanceSlider.value = Distance as! Float
+                        let report = NSString(format: "%.0f miles", self.distanceSlider.value)
+                        self.distanceLabel!.text = report as String
+                    }
+                    else
+                    {
+                        
+                    }
+                    if let DistanceUnit = (settings as AnyObject).object(forKey: "DistanceUnit" as NSString)
+                    {
+                        self.distance.selectedSegmentIndex = DistanceUnit as! Int
+                    }
+                    else
+                    {
+                        
+                    }
+                    if let FromAge = (settings as AnyObject).object(forKey: "FromAge" as NSString)
+                    {
+                        self.slider.min = FromAge as! CGFloat
+                        let report = NSString(format: "%.0f - %.0f", self.slider.min * 100.0, self.slider.max * 100.0)
+                        self.rangeLabel!.text = report as String
+                    }
+                    else
+                    {
+                        
+                    }
+                    if let ToAge = (settings as AnyObject).object(forKey: "ToAge" as NSString)
+                    {
+                        self.slider.max = ToAge as! CGFloat
+                        let report = NSString(format: "%.0f - %.0f", self.slider.min * 100.0, self.slider.max * 100.0)
+                        self.rangeLabel!.text = report as String
+                    }
+                    else
+                    {
+                        
+                    }
+                }
+            }
+        }) { (error) in
+            print(error.localizedDescription)
+        }
     }
 	
 	override func viewWillAppear(_ animated: Bool) {
@@ -63,6 +151,24 @@ class SettingsViewController: BaseViewController {
 	@IBAction func report(_ sender: RangeSlider) {
 		let report = NSString(format: "%.0f - %.0f", sender.min * 100.0, sender.max * 100.0)
 		rangeLabel!.text = report as String
+    }
+    @IBAction func SavePressed(_ sender: UISlider) {
+        self.startAnimating()
+        
+        DispatchQueue.global(qos: .background).async {
+            if let user = Auth.auth().currentUser
+            {
+                self.ref.child("users").child(user.uid).child("Settings").setValue(["Intrests": self.userSearch.selectedSegmentIndex,  "Distance": self.distanceSlider.value,  "DistanceUnit": self.distance.selectedSegmentIndex,  "FromAge": self.slider.min,  "ToAge": self.slider.max])
+                DispatchQueue.main.async {
+                    self.stopAnimating()
+                    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                    let rootViewController: AnyObject! = storyboard.instantiateViewController(withIdentifier: "rootViewController")
+                    if let window = UIApplication.shared.keyWindow{
+                        window.rootViewController = rootViewController! as? UIViewController
+                    }
+                }
+            }
+        }
     }
 }
 
